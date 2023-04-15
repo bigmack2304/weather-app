a2c5ef04d97a4272de9802de608e5d5120b5a07f
bigmack2304 <dumon7@yandex.ru>
2023-04-15 16:23:16 +0300

    изменения в обработчике хеша страницы

--- src/utils/util_HashAddressBar.ts
+++ src/utils/util_HashAddressBar.ts
@@ -1,37 +1,41 @@
+// типизация хэша страницы
 interface IHash {
     city?: string;
     lat?: string;
     lon?: string;
 }
 
+// позвращает текущий адрес страницы
 function get_href() {
     return window.location.href;
 }
 
+// возвращает хеш страницы в виде обьекта
 function get_hash_href(): IHash {
     let this_url = new URL(get_href());
-    let hash = this_url.hash;
+    let hash = decodeURIComponent(this_url.hash); // тут хеш закодирован, декодируем
 
     if (hash == "") {
         hash = "{}";
     }
 
-    hash = decodeURIComponent(hash); // тут хеш закодирован, декодируем
     if (hash.startsWith("#")) {
         hash = hash.slice(1); // убераем ненужный символ в начале
     }
+
     return JSON.parse(hash);
 }
 
+// устанавливает своиство + значение в хеш страницы
 function set_hash_href(key: string, value: string) {
     let old_hash = get_hash_href();
     let new_hash = { ...old_hash, [`${key}`]: value };
-
-    window.location.hash = JSON.stringify(new_hash);
+    window.location.hash = encodeURIComponent(JSON.stringify(new_hash));
 }
 
+// полностью отчищает хеш страницы
 function clear_hash_href() {
-    window.location.hash = JSON.stringify({});
+    window.location.hash = encodeURIComponent("{}");
 }
 
 export { get_hash_href, set_hash_href, clear_hash_href };
