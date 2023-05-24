6def8158bf5dd406d05e381aa27d8c8afb28bea8
bigmack2304 <dumon7@yandex.ru>
2023-05-24 15:24:13 +0300

    добавил middleware
    - логгер обновляния координат искомого города

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import weatherGeoReducer from "./slises/weather_lat_lon";
 import homePageReducer from "./slises/homePage";
 import { store_middleware_logger } from "./middleware/logger";
+import { store_city_pos_logger } from "./middleware/log_update_cuty_pos";
 
 const store = configureStore({
     reducer: {
@@ -15,7 +16,7 @@ const store = configureStore({
                 ignoredActionPaths: [],
                 ignoredPaths: [],
             },
-        }), // .prepend(store_middleware_logger),  // логгер изменений стейта, отключен так как есть redux dev tools
+        }).prepend(store_city_pos_logger), // .prepend(store_middleware_logger),  // логгер изменений стейта, отключен так как есть redux dev tools
 });
 
 type RootState = ReturnType<typeof store.getState>;
