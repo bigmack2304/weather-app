3145f8d9d906a72f45ed53743b7e57a68fd5244b
bigmack2304 <dumon7@yandex.ru>
2023-05-29 12:26:56 +0300

    Добавил хеш функцию
    -источник stack pverflow

--- src/utils/util_functions.ts
+++ src/utils/util_functions.ts
@@ -423,6 +423,25 @@ function add_to_macro_stack<T extends (...args: any[]) => any>(func: T, ...args:
     });
 }
 
+/*
+    генератор хэшкода по строке
+*/
+function generateHashCode(str: string, seed: number = 0): string {
+    let h1 = 0xdeadbeef ^ seed;
+    let h2 = 0x41c6ce57 ^ seed;
+
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString(16);
+}
+
 export {
     get_full_country_by_code,
     get_system_language,
@@ -448,4 +467,5 @@ export {
     update_meta_desc,
     is_mobile_screen_size,
     is_multiTuch,
+    generateHashCode,
 };
