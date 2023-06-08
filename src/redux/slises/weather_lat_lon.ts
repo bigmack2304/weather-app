b7c99ecb4849b21cca8126dad6e4bf186b752dcf
bigmack2304 <dumon7@yandex.ru>
2023-06-08 16:32:15 +0300

    upd redux action -> loc.storage
    - при изменении этих данных в сторе, будем синхронизировать с ними и
    локал сторадж

--- src/redux/slises/weather_lat_lon.ts
+++ src/redux/slises/weather_lat_lon.ts
@@ -1,5 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import React from "react";
+import { get_stprage_data, set_storage_data } from "../../appLocalStorage/appLoacalStorage";
+import { unshuft_unique_obj_to_array_force } from "../../utils/util_functions";
+import type { TStorageHistoryCity } from "../../appLocalStorage/appLoacalStorage";
 
 interface IWeatherGeoSlice {
     lat?: number;
@@ -18,6 +21,11 @@ const weatherGeoSlice = createSlice({
     initialState: initialState,
     reducers: {
         updateCity: (state, action: PayloadAction<IWeatherGeoSlice>) => {
+            let localStorageData = get_stprage_data();
+            let new_data = { name: action.payload.cityName, lat: action.payload.lat, lon: action.payload.lon };
+            let new_history = unshuft_unique_obj_to_array_force(localStorageData.history, new_data) as TStorageHistoryCity[];
+            set_storage_data({ ...localStorageData, history: [...new_history] });
+
             state.cityName = action.payload.cityName;
             state.lat = action.payload.lat;
             state.lon = action.payload.lon;
