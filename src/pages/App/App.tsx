1e5614802e663036970249e2f6be9a2cc86bb7cc
bigmack2304 <dumon7@yandex.ru>
2023-06-13 13:04:50 +0300

    upd app.tsx
    - проще сделать так

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { HomePage } from "../Home/Home";
 import { store } from "../../redux/store";
 import { Provider } from "react-redux";
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { BasePageTemplate } from "../BasePageTemplate/BasePageTemplate";
 import { NotCityFind } from "../NotCityFind/NotCityFind";
 import { NotFoundPage } from "../NotFoundPage/NotFoundPage";
@@ -17,14 +17,7 @@ function App() {
                 <HashRouter>
                     <Routes>
                         <Route path="/" element={<BasePageTemplate />}>
-                            <Route
-                                index
-                                element={
-                                    <HomeProvider>
-                                        <HomePage />
-                                    </HomeProvider>
-                                }
-                            />
+                            <Route index element={<Navigate to={"/search"} />} />
                             <Route path="not_city_find" element={<NotCityFind />} />
                             <Route
                                 path="search"
