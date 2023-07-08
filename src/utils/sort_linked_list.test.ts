a398bc42534762231c6c90e9b6cc8a33c9f23324
bigmack2304 <dumon7@yandex.ru>
2023-07-08 13:53:23 +0300

    добавил новую структуру данных (test)
    - самосортируемый двусвязный список
    
    задумывалось как аналог массиву, но более быстрый. хз, может потом
     вырежу это
    
     тут еще нужно коечто доработать
     - перезаписывать ноды с одинаковыми весами

---
+++ src/utils/sort_linked_list.test.ts
@@ -0,0 +1,120 @@
+import { sort_linked_list } from "./sort_linked_list";
+import type { ISortLinkedList } from "./sort_linked_list";
+
+describe("test sort_linked_list.ts", () => {
+    let list: ISortLinkedList<number>;
+
+    beforeEach(() => {
+        list = new sort_linked_list<number>();
+    });
+
+    test("test length", () => {
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        expect(list.lenght).toEqual(4);
+
+        list.delete(0);
+        expect(list.lenght).toEqual(3);
+    });
+
+    test("test add (no sort)", () => {
+        list.add(4);
+        list.add(2);
+        list.add(2);
+        list.add(6);
+        expect([...list.get_nodes_values()]).toEqual([4, 2, 2, 6]);
+
+        list.add(1);
+        expect([...list.get_nodes_values()]).toEqual([4, 2, 2, 6, 1]);
+    });
+
+    test("test add (sort)", () => {
+        list.add(4, "4");
+        list.add(2, "2");
+        list.add(2, "2");
+        list.add(6, "6");
+        expect([...list.get_nodes_values()]).toEqual([2, 2, 4, 6]);
+
+        list.add(1, "1");
+        expect([...list.get_nodes_values()]).toEqual([1, 2, 2, 4, 6]);
+    });
+
+    test("test delete", () => {
+        list.add(4);
+        list.add(2);
+        list.add(2);
+        list.add(6);
+        list.delete(0);
+        expect([...list.get_nodes_values()]).toEqual([2, 2, 6]);
+        list.delete(2);
+        expect([...list.get_nodes_values()]).toEqual([2, 2]);
+
+        try {
+            list.delete(10);
+        } catch (e) {
+            expect((e as Error).message).toBe("node not found");
+        }
+
+        list.delete(1);
+        expect([...list.get_nodes_values()]).toEqual([2]);
+
+        list.delete(0);
+        expect([...list.get_nodes_values()]).toEqual([]);
+    });
+
+    test("test at", () => {
+        list.add(4);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+
+        expect(list.at(0)!.value).toEqual(4);
+        expect(list.at(1)!.value).toEqual(1);
+        expect(list.at(3)!.value).toEqual(6);
+
+        expect(list.at(12)).toEqual(undefined);
+    });
+
+    test("test get_nodes_values", () => {
+        list.add(4);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+
+        let values = list.get_nodes_values();
+
+        expect(values.next().value).toEqual(4);
+        expect(values.next().value).toEqual(1);
+        expect(values.next().value).toEqual(2);
+        expect(values.next().value).toEqual(6);
+        expect(values.next().value).toEqual(undefined);
+
+        expect([...list.get_nodes_values()]).toEqual([4, 1, 2, 6]);
+    });
+
+    test("find_by_weight", () => {
+        list.add(4, "4");
+        list.add(1, "1");
+        list.add(2, "2");
+        list.add(6, "6");
+
+        expect(list.find_by_weight("2")!.index).toEqual(1);
+        expect(list.find_by_weight("2")!.node.value).toEqual(2);
+        expect(list.find_by_weight("6")!.index).toEqual(3);
+        expect(list.find_by_weight("6")!.node.value).toEqual(6);
+
+        // несуществующий вес
+        expect(list.find_by_weight("7")).toEqual(undefined);
+
+        // этот поиск работает только в отсортированном массиве
+        list = new sort_linked_list<number>();
+        list.add(4);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+
+        expect(list.find_by_weight("6")).toEqual(undefined);
+    });
+});
---
+++ src/utils/sort_linked_list.ts
@@ -0,0 +1,196 @@
+// короче вся сортировка нод завязана на свойстве weight ноды, так как значение ноды может быть не сиреализуемым.
+// поле weight это строка, значение которой задает программист.
+
+// todo: сделать чтобы ключи были уникальными, одинаковые ключи перезаписывают значения
+
+// экземпляр класса
+interface ISortLinkedList<V = any> {
+    lenght: number;
+    add(value: V, weight?: string): void;
+    delete(index: number): void;
+    at(need_index: number): TSortLinkedListNode | undefined;
+    get_nodes_values(): Generator<unknown, V | undefined, unknown>;
+    find_by_weight(findWeight: string): { node: TSortLinkedListNodeNoNull<V>; index: number } | undefined;
+}
+
+// обьект класса
+type ISortLinkedListObject<V = any> = ISortLinkedList<V> & { new (): InstanceType<typeof sort_linked_list<V>> };
+
+// тип ноды, возможно не существующей
+type TSortLinkedListNode<V = any> = InstanceType<typeof _node<V>> | null;
+
+// тип 100% существующей ноды
+type TSortLinkedListNodeNoNull<V = any> = Exclude<TSortLinkedListNode<V>, null>;
+
+const NON_WEIGHT = "--sort_linked_list > _node: NON_WEIGHT--";
+
+class _node<V = any> {
+    public constructor(val: V, weight: string) {
+        this.value = val;
+        this.weight = weight;
+    }
+
+    public value: V; // значение ноды
+    public weight: string = ""; // вес этой ноды, нужно для сортировки
+    public next_node: TSortLinkedListNode<V> = null; // ссылка на следующую ноду
+    public prev_node: TSortLinkedListNode<V> = null; // ссылка на предыдущую ноду
+}
+
+class sort_linked_list<NODE_VALUE = any> implements ISortLinkedList<NODE_VALUE> {
+    public constructor() {}
+
+    public lenght: number = 0;
+    private _first_node: TSortLinkedListNode<NODE_VALUE> = null;
+
+    // добавляет новую ноду, так чтобы веса сортировались от меньшего к большему
+    public add(value: NODE_VALUE, weight: string = NON_WEIGHT) {
+        if (this.lenght === 0) {
+            this._first_node = new _node<NODE_VALUE>(value, weight);
+        } else {
+            let before_node: TSortLinkedListNodeNoNull<NODE_VALUE> = this._first_node!; // начальная нода для сравнения
+            let new_node = new _node(value, weight); // новая нода
+            let after_node: TSortLinkedListNode<NODE_VALUE>; // следующая нода после before_node
+
+            while (true) {
+                after_node = before_node.next_node;
+
+                if (new_node.weight < before_node.weight) {
+                    let before_before_node = before_node.prev_node; // нода стоящая перед before_node
+
+                    if (before_before_node) {
+                        before_before_node.next_node = new_node;
+                        new_node.prev_node = before_before_node;
+                    }
+
+                    before_node.prev_node = new_node;
+                    new_node.next_node = before_node;
+                    this._first_node = this._get_first_node(before_node); // ??
+                    break;
+                }
+
+                if (new_node.weight == before_node.weight || new_node.weight > before_node.weight) {
+                    if (after_node !== null) {
+                        before_node = before_node.next_node!;
+                        continue;
+                    } else {
+                        new_node.prev_node = before_node;
+                        before_node.next_node = new_node;
+                        break;
+                    }
+                }
+            }
+        }
+
+        this.lenght++;
+    }
+
+    // удаляет ноду по указанному индексу
+    public delete(index: number) {
+        let need_node = this.at(index);
+
+        if (need_node == undefined) {
+            throw new Error("node not found", { cause: { lenght: this.lenght, input_index: index } });
+        }
+
+        let before_node = need_node.prev_node;
+        let after_node = need_node.next_node;
+
+        if (before_node) {
+            if (after_node) {
+                before_node.next_node = after_node;
+                after_node.prev_node = before_node;
+            } else {
+                before_node.next_node = null;
+            }
+        } else {
+            if (after_node) {
+                after_node.prev_node = null;
+                this._first_node = after_node;
+            } else {
+                this._first_node = null;
+            }
+        }
+
+        this.lenght--;
+    }
+
+    // возвращяет ноду по индексу
+    public at(need_index: number) {
+        if (this.lenght == 0) return undefined;
+        if (need_index > this.lenght) return undefined;
+        if (need_index < 0) return undefined;
+        let node: TSortLinkedListNodeNoNull<NODE_VALUE>;
+
+        // на этом этапе мы знаем что искомый номер ноды в пределах размера списка
+        // поэтому пока мы движемся в цыкле у нас всегда будет node.next_node
+
+        node = this._first_node!;
+
+        for (let i = 0; i < this.lenght; i++) {
+            if (i === need_index) {
+                return node;
+            }
+
+            node = node.next_node!;
+        }
+    }
+
+    // позволяет итерироватся по значениям нод, также можно использовать для преобразования этого списка в массив значений нод
+    public *get_nodes_values(): Generator<unknown, NODE_VALUE | undefined, unknown> {
+        if (this.lenght === 0) {
+            return;
+        }
+
+        for (let i = 0; i < this.lenght; i++) {
+            yield (this.at(i) as TSortLinkedListNodeNoNull<NODE_VALUE>).value;
+        }
+    }
+
+    // ищет ноду по указанному весу
+    // алгоритм двоичного поиска
+    public find_by_weight(findWeight: string) {
+        if (this.lenght === 0) return;
+        if (this._first_node!.weight === NON_WEIGHT) return;
+
+        let start_index = 0; // храним индекс с которого начинаем поиск в половине
+        let end_index = this.lenght; // храним индекс которым заканчивается половина
+        let current_index = Math.ceil(end_index / 2); // определяем центральный индекс
+        let node: TSortLinkedListNodeNoNull<NODE_VALUE> = this.at(current_index)!; // получаем среднюю ноду
+
+        while (true) {
+            if (node == undefined || node.weight == undefined) {
+                return; // не нашли
+            }
+
+            if (node.weight === findWeight) {
+                return { node: node, index: current_index };
+            }
+
+            if (node.weight > findWeight) {
+                end_index = current_index;
+                current_index = start_index + Math.ceil((end_index - start_index - 1) / 2);
+                node = this.at(current_index)!;
+                continue;
+            }
+
+            if (node.weight < findWeight) {
+                start_index = current_index;
+                current_index = start_index + Math.ceil((end_index - start_index) / 2);
+                node = this.at(current_index)!;
+            }
+        }
+    }
+
+    private _get_first_node(anyNode: TSortLinkedListNodeNoNull<NODE_VALUE>) {
+        let node = anyNode;
+
+        while (node.prev_node !== null) {
+            node = node.prev_node;
+        }
+
+        return node;
+    }
+}
+
+export { sort_linked_list };
+export type { ISortLinkedList, ISortLinkedListObject };
