bcbb809b52d8c9151e96bfb53a704f0c0c77ebe6
bigmack2304 <dumon7@yandex.ru>
2023-08-27 15:37:21 +0700

    добавил анимацию для блоков погоды по часам
    - для десктопа
    
    - анимация перелистывания плиток

--- src/components/City5d3hWeather/City5d3hWeather_dayWeather.tsx
+++ src/components/City5d3hWeather/City5d3hWeather_dayWeather.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, createRef, useState } from "react";
 import { decode_dataIdRender } from "./City5d3hWeather";
 import type { TresponseObjListObj } from "../../utils/fetch_5d3h_weather";
 import type { TResponse } from "../../utils/fetch_5d3h_weather";
@@ -7,6 +7,7 @@ import { HoverHint } from "../../HOC/HoverHint/HoverHint";
 import { addon_map, deg_to_compass, get_text_date, is_mobile_screen_size } from "../../utils/util_functions";
 import { IconDirection } from "../../ui/IconDirection";
 import "./City5d3hWeather.scss";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 type TCity5d3hWeather_dayWeatherProps = {
     dataIdRender: string;
@@ -18,6 +19,19 @@ type TProps = Readonly<TCity5d3hWeather_dayWeatherProps>;
 
 function City5d3hWeather_dayWeather({ dataIdRender, sorted_days_weather, weather }: TProps) {
     let day_id = Number(decode_dataIdRender(dataIdRender).index);
+    const [desctopListRef, setDesctopListRef] = useState<Map<number, React.RefObject<HTMLDivElement>>>(new Map()); // эта конструкция должна хранить зарезервированные рефы на каждый City5d3hWeather__day_time_list, однако почемуто она хранит ссылку на текущий отображаемый лист, странно но даже так все работает. Нужно для того чтобы TransitionGroup понимал что мы добавляем а что удаляем
+
+    useEffect(() => {
+        let temp = new Map<number, React.RefObject<HTMLDivElement>>();
+
+        for (let week of sorted_days_weather) {
+            if (!temp.has(week[0].dt)) {
+                temp.set(week[0].dt, createRef());
+            }
+        }
+
+        setDesctopListRef(temp);
+    }, [sorted_days_weather]);
 
     // api не предоставляет время восхода и заката для будующих дней, поэтому выщитываем его самостоятельно, исходя из текущего дня
 
@@ -51,7 +65,16 @@ function City5d3hWeather_dayWeather({ dataIdRender, sorted_days_weather, weather
         <>
             {/* разметка будет двух типов: под мобильник и десктоп */}
             {!is_mobile_screen_size() ? (
-                <>{desctop_html(day_id, sorted_days_weather, weather, calc_sunrise, calc_sunset)}</>
+                <>
+                    {desctop_html(
+                        day_id,
+                        sorted_days_weather,
+                        weather,
+                        calc_sunrise,
+                        calc_sunset,
+                        desctopListRef as Map<number, React.RefObject<HTMLDivElement>>
+                    )}
+                </>
             ) : (
                 <>{mobile_html(day_id, sorted_days_weather, weather, calc_sunrise, calc_sunset)}</>
             )}
@@ -133,53 +156,60 @@ function desctop_html(
     sorted_days_weather: TresponseObjListObj[][],
     weather: TResponse,
     calc_sunrise: (val: number, val2: number) => number,
-    calc_sunset: (val: number, val2: number) => number
+    calc_sunset: (val: number, val2: number) => number,
+    desctopListRef: Map<number, React.RefObject<HTMLDivElement>>
 ) {
     return (
-        <>
-            {sorted_days_weather[day_id].map((forecast) => {
-                return (
-                    <div key={forecast.dt} className="City5d3hWeather__day_time_weather">
-                        <span className="City5d3hWeather__day_time_time">
-                            {(function () {
-                                let text_date = get_text_date(forecast.dt * 1000);
-                                return `${text_date.hoursUTC}:${text_date.minutesUTC}`;
-                            })()}
-                        </span>
-                        <WeatherIcon
-                            weather_data={{
-                                // этот компонент изначально был расчитан на текущую погоду, поэтому там требовалось сдвигать sunrise. sunset. dt на timezone, сдесь для dt это не нужно
-                                sunrise: calc_sunrise(forecast.dt, weather!.city.timezone), // api не предостовляет информации о восходе и заходе на будующие дни, поэтому для них будем брать время восхода такоеже как и в текущем дне, для этого секундный таймштамп увеличиваем на 1 день
-                                sunset: calc_sunset(forecast.dt, weather!.city.timezone),
-                                timezone: weather!.city.timezone, // timezone нужен для корректного расчета sunrise и sunset
-                                dt: forecast.dt - weather!.city.timezone, // при расчетах WeatherIcon сдвигает это время на timezone, но в данном случае мы это невелируем, сдивигая его в противоположную сторону
-                                weather_id: forecast.weather[0].id,
-                            }}
-                        />
-                        <HoverHint hoverText={`Вероятность осадков ${addon_map(forecast.pop, 0.0, 1.0, 0, 100).toFixed(0)}%`}>
-                            <span className="City5d3hWeather__day_time_desc">{forecast.weather[0].description}</span>
-                        </HoverHint>
-                        {forecast.rain ? <span>{`${forecast.rain["3h"]} мм/3ч`}</span> : null}
-                        {forecast.snow ? <span>{`${forecast.snow["3h"]} мм/3ч`}</span> : null}
-                        {forecast.wind ? (
-                            <div className="City5d3hWeather__day_time_wnd">
-                                <span>{forecast.wind.speed.toFixed(1)} м/сек</span>
-                                <HoverHint hoverText={deg_to_compass(forecast.wind.deg)}>
-                                    <IconDirection direction={forecast.wind.deg} />
+        <TransitionGroup component={null}>
+            <CSSTransition
+                timeout={250}
+                classNames={"City5d3hWeather__day_time_list"}
+                key={sorted_days_weather[day_id][0].dt}
+                nodeRef={desctopListRef.get(sorted_days_weather[day_id][0].dt)}
+            >
+                <div className="City5d3hWeather__day_time_list" ref={desctopListRef.get(sorted_days_weather[day_id][0].dt)}>
+                    {sorted_days_weather[day_id].map((forecast) => {
+                        return (
+                            <div key={forecast.dt} className="City5d3hWeather__day_time_weather">
+                                <span className="City5d3hWeather__day_time_time">
+                                    {(function () {
+                                        let text_date = get_text_date(forecast.dt * 1000);
+                                        return `${text_date.hoursUTC}:${text_date.minutesUTC}`;
+                                    })()}
+                                </span>
+                                <WeatherIcon
+                                    weather_data={{
+                                        // этот компонент изначально был расчитан на текущую погоду, поэтому там требовалось сдвигать sunrise. sunset. dt на timezone, сдесь для dt это не нужно
+                                        sunrise: calc_sunrise(forecast.dt, weather!.city.timezone), // api не предостовляет информации о восходе и заходе на будующие дни, поэтому для них будем брать время восхода такоеже как и в текущем дне, для этого секундный таймштамп увеличиваем на 1 день
+                                        sunset: calc_sunset(forecast.dt, weather!.city.timezone),
+                                        timezone: weather!.city.timezone, // timezone нужен для корректного расчета sunrise и sunset
+                                        dt: forecast.dt - weather!.city.timezone, // при расчетах WeatherIcon сдвигает это время на timezone, но в данном случае мы это невелируем, сдивигая его в противоположную сторону
+                                        weather_id: forecast.weather[0].id,
+                                    }}
+                                />
+                                <HoverHint hoverText={`Вероятность осадков ${addon_map(forecast.pop, 0.0, 1.0, 0, 100).toFixed(0)}%`}>
+                                    <span className="City5d3hWeather__day_time_desc">{forecast.weather[0].description}</span>
+                                </HoverHint>
+                                {forecast.rain ? <span>{`${forecast.rain["3h"]} мм/3ч`}</span> : null}
+                                {forecast.snow ? <span>{`${forecast.snow["3h"]} мм/3ч`}</span> : null}
+                                {forecast.wind ? (
+                                    <div className="City5d3hWeather__day_time_wnd">
+                                        <span>{forecast.wind.speed.toFixed(1)} м/сек</span>
+                                        <HoverHint hoverText={deg_to_compass(forecast.wind.deg)}>
+                                            <IconDirection direction={forecast.wind.deg} />
+                                        </HoverHint>
+                                    </div>
+                                ) : null}
+                                <HoverHint hoverText={`Ощущается как: ${forecast.main.feels_like.toFixed(1)} °c`}>
+                                    <span className="City5d3hWeather__day_time_temp">{`${forecast.main.temp.toFixed(1)} °c`}</span>
                                 </HoverHint>
                             </div>
-                        ) : null}
-                        <HoverHint hoverText={`Ощущается как: ${forecast.main.feels_like.toFixed(1)} °c`}>
-                            <span className="City5d3hWeather__day_time_temp">{`${forecast.main.temp.toFixed(1)} °c`}</span>
-                        </HoverHint>
-                    </div>
-                );
-            })}
-        </>
+                        );
+                    })}
+                </div>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
 
 export { City5d3hWeather_dayWeather };
-
-// if (now_timestamp > sun_data.sunset.timestamp || now_timestamp < sun_data.sunrise.timestamp) {
-// if (1684443600000 > 1684531080000 || 1684443600000 < 1684472700000) {
